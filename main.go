2862d39f6b702402e86b0809f817d7f51682f21f
Jeevan Reddy <jeevan.reddy@ge.com>
2018-08-22 20:50:28 +0530

    port fix - heroku deploy

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"github.com/jeevanrd/sms-service/routers"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if(port  == "") {
+		port = "7070"
+	}
 	router := mux.NewRouter().StrictSlash(true)
 	routers.CreateAppRouter(router)
-	http.ListenAndServe(":7070", router)
+	http.ListenAndServe(":" + port, router)
 }

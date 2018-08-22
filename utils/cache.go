3fbc7c67a8d1d98539ad24c292b67dc4a91f5891
Jeevan Reddy <jeevan.reddy@ge.com>
2018-08-22 22:32:23 +0530

    Added in memory cache

--- utils/cache.go
+++ utils/cache.go
@@ -1,33 +1,43 @@
 package utils
 
 import (
-	"gopkg.in/redis.v4"
+	//"gopkg.in/redis.v4"
 	"time"
 	"github.com/patrickmn/go-cache"
+	"errors"
 )
 
-type Cache struct {
-	Client *redis.Client
+type LocalCache struct {
+	Client *cache.Cache
 }
 
-func GetCacheClient() *redis.Client {
-	cache.New(5*time.Minute, 10*time.Minute)
-	client := redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:6379",
-		Password: "", // no password set
-		DB:       0, // use default DB
-	})
-	return client
+//func GetCacheClient() *redis.Client {
+//	return cache.New(5*time.Minute, 10*time.Minute)
+//	client := redis.NewClient(&redis.Options{
+//		Addr:     "127.0.0.1:6379",
+//		Password: "", // no password set
+//		DB:       0, // use default DB
+//	})
+//	return client
+//}
+
+func GetCacheClient() *cache.Cache {
+	return cache.New(5*time.Minute, 10*time.Minute)
 }
 
-func (c *Cache) SetIntValueInCache(key string, value int64, time time.Duration) error {
-	return c.Client.Set(key, value, time).Err()
+
+func (c *LocalCache) SetIntValueInCache(key string, value int64, time time.Duration)  {
+	c.Client.Set(key, value, time)
 }
 
-func (c *Cache) GetIntValueFromCache(key string) (int64, error) {
-	return c.Client.Get(key).Int64()
+func (c *LocalCache) GetIntValueFromCache(key string) (int64, error) {
+	val, exist := c.Client.Get(key)
+	if (exist) {
+		return val.(int64), nil
+	}
+	return int64(0), errors.New("key not foun")
 }
 
-func (c *Cache) Increment(key string, value int64) {
-	c.Client.IncrBy(key, value)
+func (c *LocalCache) Increment(key string, value int64) {
+	c.Client.Increment(key, value)
 }
